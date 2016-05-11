2d9fa4efd2bf7e9263fa3862728e89021a400c10
Jonas Bengtsson <jonasb@ustwo.com>
2016-05-11 18:28:08 +0200

    Add direction activity

---
+++ app/src/main/java/net/wayfindr/demo/activity/DirectionActivity.java
@@ -0,0 +1,14 @@
+package net.wayfindr.demo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.wayfindr.demo.R;
+
+public class DirectionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction);
+    }
+}
