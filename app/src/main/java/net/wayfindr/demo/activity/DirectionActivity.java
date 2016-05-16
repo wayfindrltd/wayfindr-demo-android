486278e554e8c3d625792d3006d7bddbc0a9d42b
Jonas Bengtsson <jonasb@ustwo.com>
2016-05-16 11:23:03 +0200

    Add debug as panel to directions activity

--- app/src/main/java/net/wayfindr/demo/activity/DirectionActivity.java
+++ app/src/main/java/net/wayfindr/demo/activity/DirectionActivity.java
@@ -8,12 +8,15 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import net.wayfindr.demo.R;
 import net.wayfindr.demo.controller.DirectionsController;
 import net.wayfindr.demo.controller.NearbyMessagesController;
 import net.wayfindr.demo.controller.TextToSpeechController;
+import net.wayfindr.demo.model.DirectionMessage;
 import net.wayfindr.demo.model.Message;
 
 import java.util.HashSet;
@@ -25,9 +28,15 @@ public class DirectionActivity extends AppCompatActivity {
     private NearbyMessagesController nearbyMessagesController;
     private DirectionsController directionsController;
     private TextView messageTextView;
+    private TextView waitingForIdTextView;
+    private TextView currentMessagesTextView;
+    private Switch visitMessage1Switch;
+    private Switch visitMessage2Switch;
+    private Switch visitMessage3Switch;
     private View restartButton;
     private View loadingPanel;
     private View directionsPanel;
+    private View debugPanel;
 
     public static Intent createIntent(Context context) {
         return new Intent(context, DirectionActivity.class);
@@ -53,6 +62,7 @@ public class DirectionActivity extends AppCompatActivity {
         directionsController = new DirectionsController(textToSpeechController, savedInstanceState, new DirectionsController.Callback() {
             @Override
             public void onWaitingForIdChanged(String id) {
+                waitingForIdTextView.setText(id == null ? "" : id);
                 updateUiVisibility();
             }
 
@@ -81,17 +91,38 @@ public class DirectionActivity extends AppCompatActivity {
 
         directionsPanel = findViewById(R.id.directionsPanel);
         loadingPanel = findViewById(R.id.loadingPanel);
+        debugPanel = findViewById(R.id.debug_panel);
         messageTextView = (TextView) findViewById(R.id.message);
+        waitingForIdTextView = (TextView) findViewById(R.id.waitingForId);
+        waitingForIdTextView.setText(directionsController.getWaitingForId());
+        currentMessagesTextView = (TextView) findViewById(R.id.currentMessages);
+        visitMessage1Switch = (Switch) findViewById(R.id.visitMessage1);
+        visitMessage2Switch = (Switch) findViewById(R.id.visitMessage2);
+        visitMessage3Switch = (Switch) findViewById(R.id.visitMessage3);
         restartButton = findViewById(R.id.restart);
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                messageTextView.setText("");
-                directionsController.restart();
-                updateUiVisibility();
+                restart();
             }
         });
 
+        CompoundButton.OnCheckedChangeListener messageSwitchesListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateCurrentMessages(nearbyMessagesController.getCurrentMessages());
+            }
+        };
+        visitMessage1Switch.setOnCheckedChangeListener(messageSwitchesListener);
+        visitMessage2Switch.setOnCheckedChangeListener(messageSwitchesListener);
+        visitMessage3Switch.setOnCheckedChangeListener(messageSwitchesListener);
+
+        updateUiVisibility();
+    }
+
+    private void restart() {
+        messageTextView.setText("");
+        directionsController.restart();
         updateUiVisibility();
     }
 
@@ -103,7 +134,29 @@ public class DirectionActivity extends AppCompatActivity {
     }
 
     private void updateCurrentMessages(Set<Message> currentMessages) {
-        directionsController.setCurrentMessages(currentMessages);
+        HashSet<Message> messagesIncludingSwitches = new HashSet<>(currentMessages);
+        if (visitMessage1Switch.isChecked()) {
+            messagesIncludingSwitches.add(new DirectionMessage("1", DirectionMessage.Type.START, "Welcome to 1. Proceed to 2", "2"));
+        }
+        if (visitMessage2Switch.isChecked()) {
+            messagesIncludingSwitches.add(new DirectionMessage("2", DirectionMessage.Type.NODE, "Proceed to 3", "3"));
+        }
+        if (visitMessage3Switch.isChecked()) {
+            messagesIncludingSwitches.add(new DirectionMessage("3", DirectionMessage.Type.FINISH, "You made it", null));
+        }
+
+        directionsController.setCurrentMessages(messagesIncludingSwitches);
+
+        if (messagesIncludingSwitches.isEmpty()) {
+            currentMessagesTextView.setText("<None>");
+        } else {
+            String text = "";
+            for (Message message : messagesIncludingSwitches) {
+                if (!text.isEmpty()) text += "\n";
+                text += message;
+            }
+            currentMessagesTextView.setText(text);
+        }
     }
 
     @Override
@@ -140,6 +193,15 @@ public class DirectionActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        if (debugPanel.getVisibility() == View.VISIBLE) {
+            debugPanel.setVisibility(View.GONE);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_direction, menu);
@@ -150,7 +212,8 @@ public class DirectionActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.switch_to_debug:
-                startActivity(DebugActivity.createIntent(this));
+                item.setChecked(!item.isChecked());
+                debugPanel.setVisibility(item.isChecked() ? View.VISIBLE : View.GONE);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
